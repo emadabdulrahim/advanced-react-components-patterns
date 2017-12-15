f26f21df1c3406b683ac41fa1b252d35c5e0e84b
Emad Abdulrahim <emad90mohamad@gmail.com>
2017-12-14 17:44:03 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import React from 'react'
+import { render } from 'react-dom'
+import PropTypes from 'prop-types'
+import Hello from './Hello'
+import Switch from './switch'
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+}
+
+const withToggle = Component => {
+  function Wrapper(props, context) {
+    const toggleContext = context[TOGGLE_CONTEXT]
+
+    return <Component {...toggleContext} {...props} />
+  }
+
+  Wrapper.contextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  }
+
+  return Wrapper
+}
+
+const TOGGLE_CONTEXT = '__toggle__'
+
+const ToggleOn = ({ children }, context) =>
+  context[TOGGLE_CONTEXT].on ? children : ''
+
+ToggleOn.contextTypes = {
+  [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+}
+
+const ToggleOff = ({ children }, context) =>
+  context[TOGGLE_CONTEXT].on ? '' : children
+
+ToggleOff.contextTypes = {
+  [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+}
+
+const ToggleButton = withToggle(Switch)
+
+const CustomToggle = withToggle(({ on, toggle }, ...props) => (
+  <button onClick={toggle}>{on ? 'on' : 'off'}</button>
+))
+
+class Toggle extends React.Component {
+  static On = ToggleOn
+  static Off = ToggleOff
+  static Button = ToggleButton
+  static childContextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      on: false,
+    }
+
+    console.log('constructor')
+  }
+
+  handleClick = () => {
+    const { on } = this.state
+    this.setState({
+      on: !on,
+    })
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate')
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  getChildContext() {
+    console.log('getChildContext got called')
+    return {
+      [TOGGLE_CONTEXT]: {
+        on: this.state.on,
+        toggle: this.handleClick,
+      },
+    }
+  }
+
+  render() {
+    const { on } = this.state
+
+    return this.props.children
+  }
+}
+
+const App = () => (
+  <div style={styles}>
+    <Hello name="CodeSandbox" />
+    <Toggle>
+      <Toggle.On>The Button is on</Toggle.On>
+      <div>
+        <Toggle.Off>The Button is of</Toggle.Off>
+      </div>
+      <Toggle.Button />
+      <CustomToggle />
+    </Toggle>
+  </div>
+)
+
+render(<App />, document.getElementById('root'))
---
+++ src/switch.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Knob = styled.div`
+  position: relative;
+  width: 50%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 999px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: ${props => props.on ? '0 6px 0 -1px rgba(0, 0, 0, 0.07)' : ''};
+  transform: ${props => props.on ? 'translateX(40px)' : ''};
+  transition: all 250ms cubic-bezier(0.46, 0.07, 0.32, 1.47);
+
+  &:active {
+    padding-right: 10px;
+    height: 90%;
+    margin-left: ${props => props.on ? '-10px' : ''};
+  }
+`
+
+const Track = styled.div`
+  margin: auto;
+  position: relative;
+  width: 80px;
+  height: 40px;
+  box-sizing: content-box;
+  background-color: ${props => props.on ? '#b0ecce' : 'rgba(0, 0, 0, 0.06)'};
+  border-radius: 999px;
+  padding: 5px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  transition: background 250ms ease;
+`
+
+const Switch = ({ on, toggle, ...props }) => (
+  <Track on={on} onClick={toggle} {...props}>
+    <Knob on={on} />
+  </Track>
+)
+
+export default Switch
